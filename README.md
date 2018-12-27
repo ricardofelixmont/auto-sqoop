39e096d366606f57c303214c7a5e273a8bd565bd
Ricardo Felix <ricardofelix_mont@outlook.com>
2018-12-27 15:45:42 -0200

    Adicionado a função de criar sqoop jobs append

--- README.md
+++ README.md
@@ -1,5 +1,47 @@
 # auto-sqoop
-- Gerador de comandos SQL create table:
-	trunk/insert - work
-	append - raw e work
 
+COMO USAR:
+
+	|||---GERAR CREATE TABLES---|||
+	Precisamos gerar 1 arquivo .txt para o Scheema de cada tabela.
+	Esses arquivo devem ser colocados no mesmo diretório do 'create_tables.py'
+	exemplo: rezadod01.dbo.Agent.txt
+	
+	No arquivo.ini:
+	-path = 'diretorio onde o modulo create_tables.py está'
+	
+	O programa cria um diretório 'saida'
+	Trata os arquivos da entrada e joga na saida como .sql
+	O programa retira o cabeçalho das tabelas, portanto as tabelas precisam estar com o cabecalho
+	
+	CUIDADOS:
+	1 - Toda vez que o programa roda, ele da um append no arquivo de saida,
+	por esse movito é importante apagar o arquivo para gerar novos 'Create Tables'.
+	2 - O arquivo .txt precisa ser o mesmo do nome da tabela no SQL server.
+	ex: rezadod01.dbo.Agent.txt
+	pois esse nome é utilizado para preencher o comando create_table
+	
+	
+	|||---GERAR SQOOP JOBS---|||
+	Preencher o arquivo 'campos_importantes.csv' com:
+	Estes campos são utilizado para gerar os sqoop jobs
+	1 - nome da tabela
+	2 - chave primaria
+	3 - campo assistido
+	4 - particionar_campo_por
+	
+	Depois precisamos gerar 1 arquivo .txt para o Scheema de cada tabela.
+	Esses arquivo devem ser colocados no mesmo diretório do 'create_tables.py'
+	exemplo: rezadod01.dbo.Agent.txt
+	
+	Agora é só executar o programa e escolher a opção 3 do Menu:  
+	[3] - Formatar colunas SqoopJOB
+	- Na pasta saida se encontra o arquivo 'newSkies-append-job.txt', que contem todos
+	os sqoop jobs gerados.
+	
+	CUIDADOS: 
+	1 - Toda vez que o programa roda, ele da um append no arquivo 'newSkies-append-job.txt', 
+	por esse movito é importante apagar o arquivo para gerar novos sqoop jobs.
+	2 - O arquivo .txt precisa ser o mesmo do nome da tabela no SQL server. 
+	ex: rezadod01.dbo.Agent.txt
+	pois esse nome é utilizado para preencher o sqoop job
--- create_tables_ricardo.py
+++ create_tables_ricardo.py
@@ -1,19 +1,53 @@
 #!/usr/bin/env python3
 
 # =================COMO USAR==========================
-# Colocar os .txt que devem ser tratados na mesma pasta do programa 'create_tables.py'
-# O nome dos arquivos.txt tem que ser o mesmo nome da tabela a ser tratada, exemplo: CustomerProgramSettingType.txt
+# |||---GERAR CREATE TABLES---|||
+# Precisamos gerar 1 arquivo .txt para o Scheema de cada tabela.
+# Esses arquivo devem ser colocados no mesmo diretório do 'create_tables.py'
+#   exemplo: rezadod01.dbo.Agent.txt
+#
 # No arquivo.ini:
 #   -path = 'diretorio onde o modulo create_tables.py está'
-#   -nome_do_banco = 'rezad01'
-#   -campo_assistido = 'ano_mes int'
 #
-# Programa cria um diretório 'saida'
+# O programa cria um diretório 'saida'
 # Trata os arquivos da entrada e joga na saida como .sql
 # O programa retira o cabeçalho das tabelas, portanto as tabelas precisam estar com o cabecalho
+#
+# CUIDADOS:
+# 1 - Toda vez que o programa roda, ele da um append no arquivo de saida,
+# por esse movito é importante apagar o arquivo para gerar novos 'Create Tables'.
+# 2 - O arquivo .txt precisa ser o mesmo do nome da tabela no SQL server.
+#   ex: rezadod01.dbo.Agent.txt
+# pois esse nome é utilizado para preencher o comando create_table
+
+#
+# |||---GERAR SQOOP JOBS---|||
+# Preencher o arquivo 'campos_importantes.csv' com:
+#   Estes campos são utilizado para gerar os sqoop jobs
+#   1 - nome da tabela
+#   2 - chave primaria
+#   3 - campo assistido
+#   4 - particionar_campo_por
+#   
+# Depois precisamos gerar 1 arquivo .txt para o Scheema de cada tabela.
+# Esses arquivo devem ser colocados no mesmo diretório do 'create_tables.py'
+#   exemplo: rezadod01.dbo.Agent.txt
+# 
+# Agora é só executar o programa e escolher a opção 3 do Menu:  
+# [3] - Formatar colunas SqoopJOB
+# - Na pasta saida se encontra o arquivo 'newSkies-append-job.txt', que contem todos
+# os sqoop jobs gerados.
+# 
+# CUIDADOS: 
+# 1 - Toda vez que o programa roda, ele da um append no arquivo 'newSkies-append-job.txt', 
+# por esse movito é importante apagar o arquivo para gerar novos sqoop jobs.
+# 2 - O arquivo .txt precisa ser o mesmo do nome da tabela no SQL server. 
+#   ex: rezadod01.dbo.Agent.txt
+# pois esse nome é utilizado para preencher o sqoop job
 
 import os
 import configparser
+import csv
 
 
 conteudo = ''       # Guarda o conteudo de cada linha da tabela, que sera gravada na saida
@@ -24,8 +58,6 @@ config = configparser.ConfigParser()
 config.read(conf_file, encoding='utf8')
 
 path = config.get('path', 'path_name') # Variavel path contem o caminho
-nome_do_banco = config.get('path', 'nome_do_banco')
-campo_assistido = config.get('path', 'campo_assistido')
 
 os.system(f'ls *.txt > lista') # Cria um arquivo lista com o nome dos arquivos que tem final '.txt' no diretorio de entrada
 os.system('mkdir saida')      # Cria um diretorio 'saida' 
@@ -59,9 +91,14 @@ def formata_tabela(conteudo):
 # que estamos tratando
 def create_table_trunc_insert(conteudo, nome_da_tabela, conjunto_substituidos):
     substituidos = ''
+
+    # Pega o nome da tabela e o nome do banco, a partir do nome do arquivo que 'campos_importantes.csv'
+    single_table_name = nome_da_tabela.split('.')[2]
+    single_db_name = nome_da_tabela.split('.')[0]
+
     for substituido in conjunto_substituidos:
         substituidos += '--Esta tabela possui o tipo ' + substituido + '\n'
-    create_table = f"--{nome_do_banco}.dbo.{nome_da_tabela}\n--work\n{substituidos}\nCREATE TABLE IF NOT EXISTS {nome_do_banco}_work.{nome_da_tabela}(\n\n{formata_tabela(conteudo)})\n\n ROW format DELIMITED FIELDS TERMINATED BY '|'STORED AS TEXTFILE;\n\n"
+    create_table = f"--{nome_da_tabela}\n--work\n{substituidos}\nCREATE TABLE IF NOT EXISTS {single_db_name}_work.{single_table_name}(\n\n{formata_tabela(conteudo)})\n\n ROW format DELIMITED FIELDS TERMINATED BY '|'STORED AS TEXTFILE;\n\n"
     with open(path+'saida/Create_table_trunc_insert.sql', 'a+') as arquivo:
         arquivo.write(create_table)
         print('Create_table_trunc_insert gravado com sucesso...')
@@ -69,9 +106,14 @@ def create_table_trunc_insert(conteudo, nome_da_tabela, conjunto_substituidos):
 
 def create_table_append_raw(conteudo, nome_da_tabela, conjunto_substituidos):
     substituidos = ''
+    
+    # Pega o nome da tabela e o nome do banco, a partir do nome do arquivo que 'campos_importantes.csv'
+    single_table_name = nome_da_tabela.split('.')[2]
+    single_db_name = nome_da_tabela.split('.')[0]
+
     for substituido in conjunto_substituidos:
         substituidos += '--Esta tabela possui o tipo ' + substituido + '\n'
-    create_table = f"--{nome_do_banco}.dbo.{nome_da_tabela}\n--raw\n{substituidos}\nCREATE TABLE IF NOT EXISTS {nome_do_banco}_raw.{nome_da_tabela}(\n\n{formata_tabela(conteudo)})\n\nROW format DELIMITED FIELDS TERMINATED BY '|'STORED AS TEXTFILE;\n\n"
+    create_table = f"--{nome_da_tabela}\n--raw\n{substituidos}\nCREATE TABLE IF NOT EXISTS {single_db_name}_raw.{single_table_name}(\n\n{formata_tabela(conteudo)})\n\nROW format DELIMITED FIELDS TERMINATED BY '|'STORED AS TEXTFILE;\n\n"
     with open(path+'saida/Create_table_append.sql', 'a+') as arquivo:
         arquivo.write(create_table)
         print('Create_table_append_raw gravado com sucesso...')
@@ -79,13 +121,70 @@ def create_table_append_raw(conteudo, nome_da_tabela, conjunto_substituidos):
 
 def create_table_append_work(conteudo, nome_da_tabela, conjunto_substituidos):
     substituidos = ''
+    
+    # Pega o nome da tabela e o nome do banco, a partir do nome do arquivo que 'campos_importantes.csv'
+    single_table_name = nome_da_tabela.split('.')[2]
+    single_db_name = nome_da_tabela.split('.')[0]
+
+    campo_assistido = ''
+    with open('campos_importantes.csv', 'r') as f:
+        reader = csv.DictReader(f) # Lista de dicionarios
+        for tabela in reader:
+            if tabela['nome_tabela'] == nome_da_tabela:
+                campo_assistido = tabela['campo_assistido']
+
     for substituido in conjunto_substituidos:
         substituidos += '--Esta tabela possui o tipo ' + substituido + '\n'
-        create_table = f"--{nome_do_banco}.dbo.{nome_da_tabela}\n--work\n{substituidos}\nCREATE TABLE IF NOT EXISTS {nome_do_banco}_work.{nome_da_tabela}(\n\n{formata_tabela(conteudo)})\n\npartitioned by ({campo_assistido})\nSTORED AS PARQUET;\n\n"
+        create_table = f"--{nome_da_tabela}\n--work\n{substituidos}\nCREATE TABLE IF NOT EXISTS {single_db_name}_work.{single_table_name}(\n\n{formata_tabela(conteudo)})\n\npartitioned by ({campo_assistido})\nSTORED AS PARQUET;\n\n"
+        # O campo assistido ainda é pego do arquivo .ini
     with open(path+'saida/Create_table_append.sql', 'a+') as arquivo:
         arquivo.write(create_table)
         print('Create_table_Append_work gravado com sucesso...')
     return create_table
+
+# AUTO-SQOOP JOB GENERATOR
+def auto_sqoop_job_append(conteudo, nome_da_tabela, colunas_de_substituidos):
+
+    # Acessando o csv com a 'Primary_key' e 'Campo_assistido'
+    campo_assistido = ''
+    chave_primaria = ''
+    with open('campos_importantes.csv', 'r') as f:
+        reader = csv.DictReader(f) # Lista de dicionarios
+        for tabela in reader:
+            if tabela['nome_tabela'] == nome_da_tabela:
+                chave_primaria = tabela['chave_primaria']
+                campo_assistido = tabela['campo_assistido']
+    
+
+    # Pega o nome da tabela e o nome do banco, a partir do nome do arquivo que 'campos_importantes.csv'
+    single_table_name = nome_da_tabela.split('.')[2]
+    single_db_name = nome_da_tabela.split('.')[0]
+    
+    # Campo map-column 
+    map_col = map_column(colunas_de_substituidos)
+    # Criando a string que armazena o sqoop JOB
+    string_sqoop_job = f"--{nome_da_tabela}\n\nsqoop job \ \n   -Dhadoop.security.credential.provider.path=jceks://hdfs/credenciais/newskies.mssql.password.jceks \ \n   --create import_{single_db_name}_{single_table_name.lower()} \ \n   --meta-connect jdbc:hsqldb:hsql://clderenutb01p:16000/sqoop \ \n   -- import \ \n   --driver com.microsoft.sqlserver.jdbc.SQLServerDriver  \ \n   --connect jdbc:sqlserver://149.122.13.148:52900 \ \n   --username ADODSUSER13 \ \n   --password-alias newskies.mssql.password.alias \ \n   --table {nome_da_tabela} \ \n   --columns {retira_espacos(conteudo)} \ \n{map_col}   --hive-database {single_db_name}_raw \ \n   --hive-table {single_table_name.lower()} \ \n   --incremental append \ \n   --check-column {campo_assistido} \ \n   --delete-target-dir \ \n   --target-dir /user/hive/warehouse/{single_db_name}_raw.db/{single_table_name.lower()} \ \n   --split-by {chave_primaria} -m 20 \ \n   --direct \ \n   --verbose \ \n   --fields-terminated-by '|' \n\nsqoop job --meta-connect jdbc:hsqldb:hsql://clderenutb01p:16000/sqoop --exec import_{single_db_name}_{single_table_name.lower()}\n\n\n"
+    
+    # Gravar o Sqoop Job no arquivo newSkies-append-job.txt
+    with open(path+'saida/newSkies-append-job.txt', 'a+') as f:
+        f.write(string_sqoop_job)
+           
+    return string_sqoop_job
+
+def map_column(colunas_de_substituidos):
+    if len(colunas_de_substituidos) == 0:
+        return ''
+    else:
+        return f'   --map-column-java {substituidos(colunas_de_substituidos)} \ \n'
+
+def substituidos(colunas_de_substituidos):
+    substituidos = ''
+    for substituido in colunas_de_substituidos:
+        substituidos += f'{substituido.strip()}=Integer,'
+        """ 
+        Retorna o campo column map do sqoop JOB
+        """
+    return substituidos[:-1]
 
 # MAIN
 with open(path+'lista', 'r') as f:  # Abre a lista de arquivos
@@ -95,11 +194,10 @@ with open(path+'lista', 'r') as f:  # Abre a lista de arquivos
     print('MENU')
     print('-=' * 15)
     try: 
-        op = int(input('[1] - Gerar - Trunc/Insert\n[2] - Gerar - Append\n[3] - Formatar colunas SqoopJOB\nDigite uma opção: '))
+        op = int(input('[1] - Gerar - Trunc/Insert\n[2] - Gerar - Append\n[3] - Gerar Sqoop-job\nDigite uma opção: '))
     except Exception as e:
         print(f'Erro: {e}')
-    else:
-        print('Opção invalida, tente novamente')
+
     # Fim do Menu    
 
     for linha in f:                             # Cada linha é o nome de arquivo .txt que será aberto
@@ -110,6 +208,9 @@ with open(path+'lista', 'r') as f:  # Abre a lista de arquivos
         # Começo do tratamento (Regras de Negócio)
         conteudo = '' # Esvazia a lista
         conjunto_substituidos = set()
+        # vai ser utilizada para fazer o map column do sqoop JOB
+        colunas_de_substituidos = set()
+
         for d in data:
             a = d.split('|')
             table = a[1:6]
@@ -137,6 +238,8 @@ with open(path+'lista', 'r') as f:  # Abre a lista de arquivos
                 elif table[1].strip() == 'bit':
                     conteudo += f'{table[0].strip()} tinyint,'+ '\n'
                     conjunto_substituidos.add('bit')
+                    # Armazendo o nome das colunas que tem o tipo bit, para preencher o map reduce
+                    colunas_de_substituidos.add(table[0])
                 elif table[1].strip() == 'money':
                     conteudo += f'{table[0].strip()} decimal,' + '\n'
                     conjunto_substituidos.add('money')
@@ -151,8 +254,10 @@ with open(path+'lista', 'r') as f:  # Abre a lista de arquivos
             create_table_append_work(conteudo[:-2], nome_da_tabela, conjunto_substituidos)
             break
         # Apenas retira os espaços da tabela gerada pelo get schema
-        if op == 3:
-            print(retira_espacos(conteudo))
+        while op == 3:
+            auto_sqoop_job_append(conteudo, nome_da_tabela, colunas_de_substituidos)
+            print(f'Sqoop Job "{nome_da_tabela}" gerado com sucesso')
+            break
         # A partir daqui não é mais necessário
         # Cria um arquivo com o nome da tabela(ex: dim_accounts), grava o conteudo e fecha o arquivo
         # ref_arquivo = open(path+'saida/'+'tabela_'+linha[:-1], 'w')
